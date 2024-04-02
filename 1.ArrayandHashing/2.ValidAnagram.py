0cc160bc3329760e6336cb448fd162962d8dd8c2
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-04-02 10:25:58 +0530

    Add files via upload
    242. Valid Anagram

---
+++ 1.ArrayandHashing/2.ValidAnagram.py
@@ -0,0 +1,15 @@
+def isAnagram(s, t):
+    if len(s) != len(t):
+        return False
+
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    return countS == countT
+
+
+s = input()
+t = input()
+print(isAnagram(s, t))
