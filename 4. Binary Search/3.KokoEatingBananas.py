1be0ee1d5f015f0f77764f0a5d8c55dfce5b35d7
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-04-11 02:24:38 +0530

    Add files via upload
    875. Koko Eating Bananas

---
+++ 4. Binary Search/3.KokoEatingBananas.py
@@ -0,0 +1,23 @@
+import math
+
+def minEatingSpeed(piles, h):
+    l, r = 1, max(piles)
+    res = r
+
+    while l <= r:
+        k = (l + r) // 2
+        hours = 0
+        for pile in piles:
+            hours += math.ceil(pile / k)
+        if hours <= h:
+            res = min(res, k)
+            r = k - 1
+        else:
+            l = k + 1
+
+    return res
+
+
+piles = list(map(int, input().split()))
+h = int(input())
+print(minEatingSpeed(piles, h))
