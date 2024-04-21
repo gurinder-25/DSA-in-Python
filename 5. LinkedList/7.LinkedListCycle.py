e129291480a35352288158a1b89ef3844e6d0cb2
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-04-21 12:30:53 +0530

    Add files via upload
    141. Linked List Cycle

---
+++ 5. LinkedList/7.LinkedListCycle.py
@@ -0,0 +1,31 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while fast and fast.next:
+        if slow == fast:
+            return True
+        slow = slow.next
+        fast = fast.next.next
+    return False
+
+
+node1 = ListNode(3)
+node2 = ListNode(2)
+node3 = ListNode(0)
+node4 = ListNode(-4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2
+
+print(hasCycle(node1))
