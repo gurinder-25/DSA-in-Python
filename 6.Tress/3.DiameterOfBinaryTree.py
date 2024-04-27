d382106eee978e230ec0992f3018cc39b78068f3
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-04-28 00:48:03 +0530

    Add files via upload
    543. Diameter of Binary Tree

---
+++ 6.Tress/3.DiameterOfBinaryTree.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+def diameterOfBinaryTree(root):
+    res = 0
+
+    def dfs(root):
+        nonlocal res
+
+        if not root:
+            return 0
+        left = dfs(root.left)
+        right = dfs(root.right)
+        res = max(res, left + right)
+
+        return 1 + max(left, right)
+
+    dfs(root)
+    return res
+
+
+
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(5)
+
+print(diameterOfBinaryTree(root))
