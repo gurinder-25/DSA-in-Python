02d6136cb075dd6018bfc77221c450f0ed2020dd
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-04-30 00:05:45 +0530

    Add files via upload
    100. Same Tree

---
+++ 6.Tress/5.SameTree.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def isSameTree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q or (p.val!=q.val):
+        return False
+    return (isSameTree(p.left, q.left) and isSameTree(p.right, q.right))
+
+
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+p.right = TreeNode(3)
+
+q = TreeNode(1)
+q.left = TreeNode(2)
+q.right = TreeNode(3)
+
+print(isSameTree(p, q))
