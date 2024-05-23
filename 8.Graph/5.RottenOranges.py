32b26457cf476a82db603901d3c408e6d88224b5
Gurinder <132286862+gurinder-25@users.noreply.github.com>
2024-05-24 00:05:36 +0530

    Create 5.RottenOranges.py
    994. Rotting Oranges

---
+++ 8.Graph/5.RottenOranges.py
@@ -0,0 +1,26 @@
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = collections.deque()
+        time, fresh = 0, 0 
+
+        ROWS, COLS = len(grid), len(grid[0])
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh +=1 
+                if grid[r][c] == 2:
+                    q.append((r, c))
+        
+        directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
+
+        while q and fresh>0:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    row, col = r+dr, c+dc
+
+                    if(row in range(ROWS) and col in range(COLS) and grid[row][col]==1):
+                        grid[row][col] = 2
+                        q.append((row, col))
+                        fresh-=1 
+            time +=1 
+        return time if fresh==0 else -1   
